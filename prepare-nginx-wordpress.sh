d8b256e6f342b0107b7081c804164efd9249c2c6
Console-Junkie <32847362+Axelix2016@users.noreply.github.com>
2017-11-18 18:18:29 +0100

    Update prepare-nginx-wordpress.sh

--- prepare-nginx-wordpress.sh
+++ prepare-nginx-wordpress.sh
@@ -1,16 +1,40 @@
 #!/bin/sh
 # Prepare environment
-useradd -m -s /bin/bash wpress
-passwd wpress
-usermod -a -G docker wpress
-systemctl restart docker
+echo '############################################'
+echo '########## user wpress anlegen #############'
+sudo useradd -m -s /bin/bash wpress
+echo 'user <wpress> erfolgreich angelegt!'
+# sudo userdel -f -r wpress #to remove it.
+sudo passwd wpress
+echo 'password für user <wpress> erfolgreich geändert!'
+echo '############################################'
+sudo usermod -a -G docker wpress
+echo 'user <wpress> erfolgreich zu gruppe <docker> hinzugefügt!'
+echo '############################################'
+sudo systemctl restart docker
+echo 'docker erfolgreich neu gestartet!'
+echo '############################################'
+sudo mkdir -p /home/wpress/wordpress-compose
+echo 'verzeichnis /home/wpress/wordpress-compose erfolreich angelegt!'
+echo '############################################'
+sudo touch /home/wpress/wordpress-compose/docker-compose.yml
+echo 'datei /home/wpress/wordpress-compose/docker-compose.yml erfolreich angelegt!'
+echo '############################################'
+sudo mkdir -p /home/wpress/wordpress-compose/nginx/
+echo 'verzeichnis /home/wordpress-compose/wpress/nginx erfolreich angelegt!'
+sudo mkdir -p /home/wpress/wordpress-compose/db-data/
+echo 'verzeichnis /home/wpress/wordpress-compose/db-data erfolreich angelegt!'
+sudo mkdir -p /home/wpress/wordpress-compose/logs/nginx/
+echo 'verzeichnis /home/wpress/wordpress-compose/logs/nginx erfolreich angelegt!'
+sudo mkdir -p /home/wpress/wordpress-compose/wordpress/
+echo 'verzeichnis /home/wpress/wordpress-compose/wordpress erfolreich angelegt!'
+echo '############################################'
+sudo touch /home/wpress/wordpress-compose/nginx/wordpress.conf
+echo 'datei /home/wpress/wordpress-compose/nginx/wordpress.conf erfolreich angelegt!'
+echo '############################################'
+cd /home/wpress/
+sudo tree
+echo '############################################'
 su - wpress
-mkdir -p wordpress-compose
-cd wordpress-compose/
-touch docker-compose.yml
-mkdir -p nginx/
-mkdir -p db-data/
-mkdir -p logs/nginx/
-mkdir -p wordpress/
-touch nginx/wordpress.conf
 ### eof ##################
+
